faed24a15d49735f4c9c6f760ae01356049bd41f
Veros Kaplan <VerosK@users.noreply.github.com>
2017-09-01 17:04:34 +0200

    add ftp + TLS

--- tests/setup-testbed.sh
+++ tests/setup-testbed.sh
@@ -7,3 +7,27 @@ yum install docker docker-compose -y
 systemctl start docker
 chmod a+rw /var/run/docker.sock
 
+( mkdir -p /etc/pki/ftp;
+  cd /etc/pki/ftp; 
+  if [ ! -f ftp.pem ]; then
+      openssl req -x509 -newkey rsa:4096 \
+		-keyout ftp.key -out ftp.crt \
+	        -days 365 -nodes -subj '/CN=192.168.9.9/'
+      ln -sf ftp.crt ca.crt
+  fi
+)
+
+cat <<EOF
+Now, start the containers
+
+    vagrant ssh
+    cd /vagrant/tests
+    docker-compose up
+
+And run ftp from local machine
+
+    ftp  192.168.9.9
+    user: test
+    pass: test
+
+EOF
